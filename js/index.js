dec11585017cf9d786f0436b648d08210c279c18
OliveLeb <lebel.olivier@wanadoo.fr>
2020-11-09 09:19:49 +0100

    html structure

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 window.addEventListener('load', () => {
 
-    (function () {
+
+
+  //  (function () {
 
 
         const switchMode = document.querySelector('#switch');
         const cssFile = document.querySelector('link[href=\'css/keyboard-light.css\']');
 
-
-
-       switchMode.addEventListener('click', ()=> {
+        switchMode.addEventListener('click', ()=> {
            switchMode.checked ? cssFile.setAttribute('href','css/keyboard-dark.css') : cssFile.setAttribute('href','css/keyboard-light.css');
         })
-    })();
+  //  })();
 
+        const toggleKeyboard = document.querySelector('.toggleBtn');
+        const keyboard = document.querySelector('.keyboard');
+        toggleKeyboard.addEventListener('click', () => {
+            keyboard.classList.toggle('active');
+        })
 
-})
+});
